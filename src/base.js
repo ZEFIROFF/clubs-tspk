a24b8495c2c83a6f9070f13a88867a5309fffcda
Денис Зефиров <55986656+d3pii@users.noreply.github.com>
2021-06-27 15:43:18 +0400

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 export const base = firebase.initializeApp({
-    apiKey: "AIzaSyDd7dcWQFxwEG91D7cWPzc2iMc49mmkdlM",
-    authDomain: "clubs-tspk.firebaseapp.com",
-    databaseURL: "https://clubs-tspk-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "clubs-tspk",
-    storageBucket: "clubs-tspk.appspot.com",
-    messagingSenderId: "501534920487",
-    appId: "1:501534920487:web:7c71b862e14d6d6c6de302"
+    apiKey: "SHAR0V-PID0RASS",
+    authDomain: "SHAR0V-PID0RASS",
+    databaseURL: "SHAR0V-PID0RASS",
+    projectId: "SHAR0V-PID0RASS",
+    storageBucket: "SHAR0V-PID0RASS",
+    messagingSenderId: "SHAR0V-PID0RASS",
+    appId: "SHAR0V-PID0RASS"
 });
 export default firebase;
